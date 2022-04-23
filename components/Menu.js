2d920762114a790ebd478ab0c2d5ec13c8202387
Paul Yeboah Asamoah <yeboaasamoah@gmail.com>
2022-04-23 15:16:40 +0000

    added icons in header

--- components/Menu.js
+++ components/Menu.js
@@ -1,10 +1,10 @@
 const Menu = () => {
     return(
-        <div className="lg:p-8 p-4 flex lg:flex-row lg:justify-center flex-col justify-between items-center bg-gray-800 text-white overflow-y-auto">
+        <div className="lg:p-3 p-7 flex lg:flex-row lg:justify-center flex-col justify-between items-center bg-gray-800 text-white overflow-y-auto">
             <div className="m-2 font-medium md:text-lg text-xs">
                 ORDER FOR DELIVERY, PICK UP OR DINE-IN
             </div>
-            <button className="outline-none text-xl m-2 py-3 lg:py-3 px-2 lg:px-10 w-2/4 lg:w-auto bg-yellow-300 hover:bg-yellow-400 focus:ring-4 focus:ring-yellow-600 dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800 font-medium rounded-3xl">
+            <button className="outline-none text-xl m-2 py-3 lg:py-3 lg:px-10 w-2/4 lg:w-auto bg-yellow-300 hover:bg-yellow-400 focus:ring-4 focus:ring-yellow-600 dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800 font-medium rounded-3xl">
                 Place Order
             </button>
         </div>
--- components/UserInterface/Header.js
+++ components/UserInterface/Header.js
@@ -1,10 +1,15 @@
 import Link from "next/link";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserCircle } from '@fortawesome/free-regular-svg-icons';
+import { faBars  } from '@fortawesome/free-solid-svg-icons';
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+
 const Header = () => {
     return(           
-        <nav className="bg-white lg:p-10 p-3 sticky fixed mt-0 w-full z-10 top-0">
+        <nav className="bg-white lg:p-12 p-3 sticky fixed mt-0 w-full z-10 top-0">
             <div className="lg:container lg:mx-auto flex flex-wrap lg:justify-between justify-between items-center">
-                <div className="lg:hidden">
+                <div className="hidden">
                     <Link href="/menu">
                         <a className="text-yellow-300 no-underline hover:no-underline font-extrabold">
                             <span className="pl-2"><i className="em em-grinning">Menu</i></span>
@@ -12,21 +17,29 @@ const Header = () => {
                     </Link>
                 </div>
                 
-                <div className="ml-10">
+                <div className="lg:hidden">
                     <Link href="/menu">
                         <a className="text-yellow-300 no-underline hover:no-underline font-extrabold">
-                            <span className="text-3xl"><i className="em em-grinning">PARPKIN</i></span>
+                            <FontAwesomeIcon icon = {faBars} style={{width: '80px', height: '30px', color: 'gray' }}  />
+                        </a>    
+                    </Link>
+                </div>
+                
+                <div className="ml">
+                    <Link href="/menu">
+                        <a className="text-yellow-300 no-underline hover:no-underline font-extrabold">
+                            <span className="text-5xl"><i className="em em-grinning">PARPKIN</i></span>
                         </a>
                     </Link>
 
                     <Link href="/menu">
-                        <a className="lg:inline-block hidden lg:mx-10 lg:py-2 lg:px-4 text-gray-800 no-underline">
+                        <a className="lg:inline-block hidden lg:mx-10 lg:py-2 lg:px-4 text-gray-800 no-underline font-medium text-lg">
                             Menu
                         </a>
                     </Link>
 
                     <Link href="/menu">
-                        <a className="lg:inline-block hidden lg:py-2 lg:px-4 text-gray-800 no-underline">
+                        <a className="lg:inline-block hidden lg:py-2 lg:px-4 text-gray-800 no-underline font-medium text-lg">
                             Deals
                         </a>
                     </Link>
@@ -36,16 +49,17 @@ const Header = () => {
                     <ul className="list-reset flex justify-between flex-1 lg:flex-none items-center">
                         <li className="mr-3">
                             <Link href="/menu">
-                                <a className="lg:inline-block hidden text-gray-800 no-underline py-2 px-4">
-                                    Sign in
+                                <a className="lg:inline-block hidden text-gray-800 no-underline font-medium text-xl py-2 px-4">
+                                    <FontAwesomeIcon icon = {faUserCircle} style={{width: '40px', color: 'gray' }}  />
+                                    <span className="ml-3">Sign in</span>
                                 </a>
                             </Link>
                         </li>
-
+                        
                         <li className="mr-3">
                             <Link href="/menu">
-                                <a className="lg:inline-block text-gray-800 no-underline py-2 px-4">
-                                    Cart
+                                <a className="lg:inline-block text-gray-800 no-underline font-medium text-lg py-2 px-4">
+                                    <FontAwesomeIcon icon = {faCartShopping} style={{width: '80px', height: '30px', color: 'gray' }}  />
                                 </a>
                             </Link>
                         </li>
